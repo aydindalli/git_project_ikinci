7902e7a9e3ceee81c7d9c0523f5912456f8e673c
aydindalli <51508555+aydindalli@users.noreply.github.com>
2020-08-09 22:19:24 +0200

    Update Deneme.java
    github da degisiklik yaptim

--- src/git_project_ikinci/Deneme.java
+++ src/git_project_ikinci/Deneme.java
@@ -5,6 +5,7 @@ public class Deneme {
 	public static void main(String[] args) {
 		System.out.println("Ikinci git projem.");
 		System.out.println("Tabii bijwerken=guncellemek");
+		System.out.println("remote degisikligi");
 
 	}
 
